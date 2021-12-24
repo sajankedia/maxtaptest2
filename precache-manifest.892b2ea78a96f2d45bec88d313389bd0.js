e21f6828c5474e936e33faaec3229b9a2dbaace3
sajankedia <sajankedia@gmail.com>
2021-12-24 11:31:32 +0530

    Updates

---
+++ precache-manifest.892b2ea78a96f2d45bec88d313389bd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e50d8dcf17a0283471377e0a55aa2916",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3c5e98197ae8d7e0c4dc",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "61d6a069d0622ae36942",
+    "url": "./static/js/2.ce6d1830.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ce6d1830.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c5e98197ae8d7e0c4dc",
+    "url": "./static/js/main.aaf72cb2.chunk.js"
+  },
+  {
+    "revision": "244ccfd0639306200910",
+    "url": "./static/js/runtime-main.705fc549.js"
+  }
+]);
